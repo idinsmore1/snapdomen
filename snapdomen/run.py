837f46a7edbd04c0def868b1ac0c088301061c13
irdinsmore1 <irdinsmore1@geisinger.edu>
2022-09-13 11:25:48 -0400

    added dicomseries class for use by all functions

---
+++ snapdomen/run.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+    """
+    Main CLI runner that combines the effects of all the applications in snapdomen\n
+    :return: None
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
